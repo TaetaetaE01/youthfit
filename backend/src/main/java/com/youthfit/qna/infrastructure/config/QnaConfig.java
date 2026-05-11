b3f6434ddbdea09fe3ce9e3e76458d076093cce8
TaetaetaE01 <gogo589@naver.com>
2026-05-11 23:58:25 +0900

    feat(qna): KeywordBoostProperties + application.yml 키 추가

--- backend/src/main/java/com/youthfit/qna/infrastructure/config/QnaConfig.java
+++ backend/src/main/java/com/youthfit/qna/infrastructure/config/QnaConfig.java
@@ -5,6 +5,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({OpenAiQnaProperties.class, QnaProperties.class})
+@EnableConfigurationProperties({OpenAiQnaProperties.class, QnaProperties.class, KeywordBoostProperties.class})
 public class QnaConfig {
 }
